b577174f777faec90adb4a490a20261e6aa8535b
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2019-07-09 14:47:21 +0200

    added solutions to exercise 1 (partial)

---
+++ exercise1/app3.R
@@ -0,0 +1,40 @@
+ui <- fluidPage(
+  titlePanel("Workshop - Example 1 – Basic  Histogram"),
+  sidebarLayout(
+    sidebarPanel(
+      numericInput(inputId = "n",
+                   label = "Number of observations",
+                   value = 1000), 
+      numericInput(inputId = "bins",
+                   label = "Number of bins",
+                   value = 25)
+      ),  
+    mainPanel(
+      plotOutput("plot"), 
+      textOutput("hello"),
+      textOutput("params")
+    )
+  )
+)
+
+server <- function(input, output) {
+  data <- reactive({
+    x <- rnorm(input$n)
+    x })
+
+  output$plot <- renderPlot({
+    hist(data(), input$bins, main = "",  xlab = "x")
+  })
+
+  output$hello <- renderText({
+    "Hello world!"
+  })
+  output$params <- renderText({
+    paste0("Parameters: ",
+           " n = ", input$n, "; bins: ", input$bins) 
+  })
+}
+
+
+shinyApp(ui = ui, server = server)
+
